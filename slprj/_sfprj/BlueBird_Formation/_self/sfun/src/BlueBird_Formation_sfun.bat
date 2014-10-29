84b8d162d9030e3d64f435de826e698009008432
BlueBird <bluebirdhouse@icloud.com>
2014-10-29 13:44:14 +0800

    Revert "这一批改变的比较多"
    This reverts commit f4a84425e925f75dcfe87d4a104b2a1888a4a74e.

--- slprj/_sfprj/BlueBird_Formation/_self/sfun/src/BlueBird_Formation_sfun.bat
+++
@@ -1,1 +0,0 @@
-"E:\MATLAB\R2014a\sys\lcc64\lcc64\bin\lccmake.exe" -f BlueBird_Formation_sfun.lmk
--- slprj/_sfprj/simiam_AvoidObstacle_PrintFromOb/_self/sfun/src/simiam_AvoidObstacle_PrintFromOb_sfun.bat
+++
@@ -1,1 +0,0 @@
-"E:\MATLAB\R2014a\sys\lcc64\lcc64\bin\lccmake.exe" -f simiam_AvoidObstacle_PrintFromOb_sfun.lmk
